43df997bcf5b9bfc08fe30d08cce8966260ac24c
Vinicius De Carvalho <74793351+carvalhov@users.noreply.github.com>
2022-09-21 00:02:45 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+#fibonnaci_sequence
+import random
+
+fibonnaci(random.randrange(0, 15))
+fibonnaci(random.randrange(0, 15))
+fibonnaci(random.randrange(0, 15))
+fibonnaci(random.randrange(0, 15))
+fibonnaci(random.randrange(0, 15))
